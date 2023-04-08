ee3a6ba7f598bca8d2140edf30fcdc1b3d0434c2
Luis Felipe A. Pimenta <luisfelipearaujopimenta@gmail.com>
2023-04-08 11:27:48 -0300

    teste mysql

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import 'views/usuarios/myapp.dart';
-void main() {
+import 'package:mysql1/mysql1.dart/';
+ void main() async {
+  var conn = await MySqlConnection.connect(
+      ConnectionSettings(
+          host: 'localhost',
+          port: 3306,
+          user: 'root',
+          password: '',
+          db: 'dart'
+      ),
+  );
+  var resultado = await conn.query("Select * from usuarios");
+  print(resultado);
   runApp(const MyApp());
 }
 
